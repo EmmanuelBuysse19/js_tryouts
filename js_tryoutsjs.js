092f65b0c9d7cd751c2deea348569e7d161b0bb2
EmmanuelBuysse19 <65047513+EmmanuelBuysse19@users.noreply.github.com>
2022-02-10 09:27:05 -0600

    Add files via upload
    Second commit

---
+++ js_tryoutsjs.js
@@ -0,0 +1,8 @@
+// this is a simple variable plus a formatted string
+let dog = 'sammy';
+let age = 31
+console.log(`${dog} is ${age} years old`);
+// reassigned the variables
+dog = "Kika";
+age = 9;
+console.log(`${dog} is ${age} years old`);
